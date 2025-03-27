6b8027b2dd01eae90ea2f0164ed47cb849ef3cad
aadarsh0507 <infor0507@gmail.com>
2025-03-27 13:29:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import "./App.css";
 import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
+// import Login from "./components/Login/Login";
+// import Signup from "./components/Signup/Signup";
 import UserManagement from "./components/UserManagement/UserManagement";
 import Dashboard from "./components/Dashboard/dashboard";
 import Reprint from "./components/Home/Reprint";
@@ -20,9 +20,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/Login" element={<Login />} />
+{/*           <Route path="/Login" element={<Login />} /> */}
           <Route path="/Home" element={<Home />} />
-          <Route path="/Signup" element={<Signup />} />
+{/*           <Route path="/Signup" element={<Signup />} /> */}
           <Route path="/usermanagement" element={<UserManagement />} />
           <Route path="/reprint" element={<Reprint />} />
           <Route path="/report" element={<ReportScreen />} />
